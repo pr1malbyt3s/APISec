4ebd74e074c9427f26146a3c45de1054ce049e9c
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2021-12-09 17:32:52 -0500

    Added bcrypt support for user cred checks

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
     user = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    salt = db.Column(db.String(64), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,22 @@
 from app import app
 from flask import jsonify, request
 from functools import wraps
+from app.models import User
 import base64
+import bcrypt
 
 def basic_auth_check(auth_header:str) -> bool:
-    user = "aaron.williams"
-    password ="iloveyou"
-    encoded_credentials = auth_header.split()[-1].encode('utf-8')
-    if encoded_credentials == base64.b64encode((user + ":" + password).encode('utf-8')):
-        return True
+    encoded_credentials = auth_header.split()[-1]
+    decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+    creds = decoded_credentials.split(":")
+    user, password = creds[0], creds[1]
+    if(user and password):
+        auth_user = User.query.filter_by(user=user).first()
+        auth_pass = password.encode('utf-8')
+        auth_salt = auth_user.salt.encode('utf-8')
+        hashed_pass = bcrypt.hashpw(auth_pass, auth_salt).decode('utf-8')
+        if(hashed_pass == auth_user.password):
+            return True
     return False
 
 def basic_auth_required(func):
